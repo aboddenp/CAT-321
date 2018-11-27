8e66cd16b1918f6789bbd211f0bd4f234ebc325b
agnesrhee <32945942+agnesrhee@users.noreply.github.com>
2018-11-27 08:48:15 -0500

    Update telRec.py
    updated everything with the proper attributes that were given (adding stuff like course, x, y, z, pressure, etc)

--- src/DataBases/telRec.py
+++ src/DataBases/telRec.py
@@ -1,33 +1,45 @@
 class telRec:
 	#don't forget to fetch before you code!!!
 	def __init__(self, dict):
+		self.time = dict.get('Time')
 		self.lat = dict.get('Latitude')
 		self.long = dict.get('Longitude')
+		self.course = dict.get('Course')
 		self.alt = dict.get('Altitude')
-		self.temp = dict.get('Temperature')
 		self.hum = dict.get('Humidity')
-		self.roll = dict.get('Roll')
-		self.pitch = dict.get('Pitch')
-		self.yaw = dict.get('Yaw')
+		self.temp = dict.get('Temperature')
+		self.pressure = dict.get('Pressure')
+		self.roll = dict.get('roll')
+		self.pitch = dict.get('pitch')
+		self.yaw = dict.get('yaw')
+		self.x = dict.get('x')
+		self.y = dict.get('y')
+		self.z  = dict.get('z')
+		self.speed = dict.get('Speed')
 		self.FC = 0
 
 	def __str__(self):
 		str = "Telemetry Record: \n"
 		str+= "--"*10 
-		str += f"\nLatitude = {self.lat}\nLongitude = {self.long}\nAltitude = {self.alt}\n\
-Temperature = {self.temp}\nHumidity = {self.hum}\n\
-Roll = {self.roll}\nPitch = {self.pitch}\nYaw = {self.yaw}\nFC = {self.FC}"
+		str += f"\nTime = {self.time}\nLatitude = {self.lat}\nLongitude = {self.long}\nCourse = {self.course}\n\
+		Altitude = {self.alt}\nHumidity = {self.hum}\nTemperature = {self.temp}\nPressure = {self.pressure}\n \
+		Roll = {self.roll}\nPitch = {self.pitch}\nYaw = {self.yaw}\nX = {self.x}\nY = {self.Y}\nZ = {self.z}\n\
+		Speed = {self.speed}\nFC = {self.FC}"
 		return str
 	def __repr__(self):
 		str = "Telemetry Record: \n"
 		str+= "--"*10 
-		str += f"\nLatitude = {self.lat}\nLongitude = {self.long}\nAltitude = {self.alt}\n\
-Temperature = {self.temp}\nHumidity = {self.hum}\n\
-Roll = {self.roll}\nPitch = {self.pitch}\nYaw = {self.yaw}\nFC = {self.FC}"
+		str += f"\nTime = {self.time}\nLatitude = {self.lat}\nLongitude = {self.long}\nCourse = {self.course}\n\
+		Altitude = {self.alt}\nHumidity = {self.hum}\nTemperature = {self.temp}\nPressure = {self.pressure}\n \
+		Roll = {self.roll}\nPitch = {self.pitch}\nYaw = {self.yaw}\nX = {self.x}\nY = {self.Y}\nZ = {self.z}\n\
+		Speed = {self.speed}\nFC = {self.FC}"
 		return str 
 
 	
 #the following methods are all similar, just for different attributes
+
+	def setTime (self, time):
+		self.time = time
 
 	def setLatitude (self,lat):
 		#dict['latitude'] = lat
@@ -37,18 +49,24 @@ Roll = {self.roll}\nPitch = {self.pitch}\nYaw = {self.yaw}\nFC = {self.FC}"
 		#telRec['longitude'] = lon
 		self.lon  = lon
 
+	def setCourse (self, course):
+		self.course = course	
+
 	def setAltitude (self,alt):
 		#dict['altitude'] = alt
 		self.alt = alt
 
+	def setHumidity (self,hum):
+		#dict['humidity'] = hum
+		self.hum  = hum		
+
 	def setTemperature (self,temp):
 		#dict['temperature'] = temp
 		self.temp  = temp
-	
-	def setHumidity (self,hum):
-		#dict['humidity'] = hum
-		self.hum  = hum
-	
+
+	def setPressure (self, pressure):
+		self.pressure = pressure
+
 	def setRoll (self,roll):
 		#dict['roll'] = roll
 		self.roll  = roll
@@ -60,6 +78,18 @@ Roll = {self.roll}\nPitch = {self.pitch}\nYaw = {self.yaw}\nFC = {self.FC}"
 	def setYaw (self,yaw):
 		#dict['yaw'] = yaw
 		self.yaw = yaw 
+
+	def setX (self, x):
+		self.x = x
+
+	def setY (self, y):
+		self.y = y
+
+	def setZ (self, z):
+		self.z = z
+
+	def setSpeed (self, speed):
+		self.speed = speed
 
 	def addFC (self,faultC):
 		self.FC = faultC
