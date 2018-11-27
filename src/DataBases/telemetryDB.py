5a56dc0c6d24d16b4b44720250d80b818f10cc8f
agnesrhee <32945942+agnesrhee@users.noreply.github.com>
2018-11-27 08:10:42 -0500

    Update telemetryDB.py
    added self everywhere to go with the constructor

--- src/DataBases/telemetryDB.py
+++ src/DataBases/telemetryDB.py
@@ -6,20 +6,20 @@ class telemetryDB:
 
 	#We are adding an entire telemetry record each time and also incrementing our counter
 	def addRecord(t):
-		telemetry.append(telRec)
-		recordCount += 1
+		self.telemetry.append(telRec)
+		self.recordCount += 1
 
 	#this gets the total number of records, or our counter recordCount
 	def getNumRec():
-		return recordCount
+		return self.recordCount
 
 	#this gives us the most recent record which is in the back of the list
 	def getLastRecord():
-		return telemetry(recordCount - 1)
+		return self.telemetry(recordCount - 1)
 
 	#this allows us to look at any record we want
 	def getRecord(i):
-		return telemetry(i)
+		return self.telemetry(i)
 
 
 
