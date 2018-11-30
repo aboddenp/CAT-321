71b9409a005ec00fd8a18a10652fb9bd721b3587
agnesrhee <32945942+agnesrhee@users.noreply.github.com>
2018-11-30 11:52:58 -0500

    Update telRec.py

--- src/DataBases/telRec.py
+++ src/DataBases/telRec.py
@@ -4,35 +4,28 @@ class telRec:
 		self.time = dict.get('Time')
 		self.lat = dict.get('Latitude')
 		self.long = dict.get('Longitude')
-		self.course = dict.get('Course')
 		self.alt = dict.get('Altitude')
-		self.hum = dict.get('Humidity')
+		self.roll = dict.get('roll')
+		self.pitch = dict.get('pitch')	
+		self.yaw = dict.get('yaw')	
 		self.temp = dict.get('Temperature')
 		self.pressure = dict.get('Pressure')
-		self.roll = dict.get('roll')
-		self.pitch = dict.get('pitch')
-		self.yaw = dict.get('yaw')
-		self.x = dict.get('x')
-		self.y = dict.get('y')
-		self.z  = dict.get('z')
-		self.speed = dict.get('Speed')
+		self.hum = dict.get('Humidity')
 		self.FC = 0
 
 	def __str__(self):
 		str = "Telemetry Record: \n"
 		str+= "--"*10 
-		str += f"\nTime = {self.time}\nLatitude = {self.lat}\nLongitude = {self.long}\nCourse = {self.course}\n\
-		Altitude = {self.alt}\nHumidity = {self.hum}\nTemperature = {self.temp}\nPressure = {self.pressure}\n \
-		Roll = {self.roll}\nPitch = {self.pitch}\nYaw = {self.yaw}\nX = {self.x}\nY = {self.y}\nZ = {self.z}\n\
-		Speed = {self.speed}\nFC = {self.FC}"
+		str += f"\nTime = {self.time}\nLatitude = {self.lat}\nLongitude = {self.long}\n\
+		Altitude = {self.alt}\nRoll = {self.roll}\nPitch = {self.pitch}\nYaw = {self.yaw}\n\
+		Temperature = {self.temp}\nPressure = {self.pressure}\nHumidity = {self.hum}\n"
 		return str
 	def __repr__(self):
 		str = "Telemetry Record: \n"
 		str+= "--"*10 
-		str += f"\nTime = {self.time}\nLatitude = {self.lat}\nLongitude = {self.long}\nCourse = {self.course}\n\
-		Altitude = {self.alt}\nHumidity = {self.hum}\nTemperature = {self.temp}\nPressure = {self.pressure}\n \
-		Roll = {self.roll}\nPitch = {self.pitch}\nYaw = {self.yaw}\nX = {self.x}\nY = {self.Y}\nZ = {self.z}\n\
-		Speed = {self.speed}\nFC = {self.FC}"
+		str += f"\nTime = {self.time}\nLatitude = {self.lat}\nLongitude = {self.long}\n\
+		Altitude = {self.alt}\nRoll = {self.roll}\nPitch = {self.pitch}\nYaw = {self.yaw}\n\
+		Temperature = {self.temp}\nPressure = {self.pressure}\nHumidity = {self.hum}\n"
 		return str
  
 	
@@ -49,23 +42,9 @@ class telRec:
 		#telRec['longitude'] = lon
 		self.lon  = lon
 
-	def setCourse (self, course):
-		self.course = course	
-
 	def setAltitude (self,alt):
 		#dict['altitude'] = alt
 		self.alt = alt
-
-	def setHumidity (self,hum):
-		#dict['humidity'] = hum
-		self.hum  = hum		
-
-	def setTemperature (self,temp):
-		#dict['temperature'] = temp
-		self.temp  = temp
-
-	def setPressure (self, pressure):
-		self.pressure = pressure
 
 	def setRoll (self,roll):
 		#dict['roll'] = roll
@@ -77,19 +56,18 @@ class telRec:
 
 	def setYaw (self,yaw):
 		#dict['yaw'] = yaw
-		self.yaw = yaw 
+		self.yaw = yaw 			
 
-	def setX (self, x):
-		self.x = x
+	def setTemperature (self,temp):
+		#dict['temperature'] = temp
+		self.temp  = temp
 
-	def setY (self, y):
-		self.y = y
+	def setPressure (self, pressure):
+		self.pressure = pressure
 
-	def setZ (self, z):
-		self.z = z
-
-	def setSpeed (self, speed):
-		self.speed = speed
+	def setHumidity (self,hum):
+		#dict['humidity'] = hum
+		self.hum  = hum	
 
 	def addFC (self,faultC):
 		self.FC = faultC
